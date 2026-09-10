2d660459b07525beab049b93d79fe53c6eefdb65
sultan <sultanshah101004@gmail.com>
2026-09-10 16:56:04 +0500

    Refactor home function and streamline app run

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():
-
-return "Hello from CI/CD!"
+    return "Hello from CI/CD!"
 
 if __name__ == "__main__":
-
-app.run(host="0.0.0.0",
-
-port=5000)
+    app.run(host="0.0.0.0",port=5000)
