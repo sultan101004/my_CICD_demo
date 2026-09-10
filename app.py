04651bf6903d1acbd754003316138a84991ec287
sultan101004 <sultanshah101004@gmail.com>
2026-09-10 16:57:05 +0500

    wala sai kya ha

--- app.py
+++ app.py
@@ -3,13 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-
 def home():
+    return "Hello from CI/CD!"
 
-return "Hello from CI/CD!"
-
-if __name__ == "__main__":
-
-app.run(host="0.0.0.0",
-
-port=5000)
+if __name__ == "__main__": 
+    app.run(host="0.0.0.0", port=5000)
